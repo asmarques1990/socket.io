4e3a22bd2698939a4e9181cdc892fe5e283bc1f7
asmarques1990 <31327214+asmarques1990@users.noreply.github.com>
2017-08-25 00:14:48 +0100

    Socket.IO with nodeJS and Javascript

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# socket.io
+# Before Running:
+On shell: npm install socket.io --save
+On shell: npm install express --save
+
+# How to Run:
+On shell: node server.js
+On browser: http://localhost:3000/
---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app 	= express();
+
+var http = require('http').Server(app);
+var io   = require('socket.io')(http);
+
+// Needed for loading of external css and js files from index.html
+app.use(express.static(__dirname));
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+// When the node server is started, the number of clicks is started with 1
+var clicks = 1;
+
+// Code to handle io connection from clients
+io.on('connection', function(socket){
+  	console.log('New User Connected: ' + socket.id);
+
+  	// Only send the number of clicks to the new socket connection
+	socket.emit('counterValue', clicks);
+
+	// When the socket connection increment the value, emit for all the users connected to this server
+	socket.on('incrementCounterValue', function(msg) {
+		clicks++;
+		io.emit('counterValue', clicks);
+  	});
+
+});
+
+// Set the server (localhost) to listen in port 3000
+http.listen(3000, function(){
+  console.log('Listening on *:3000');
+});
